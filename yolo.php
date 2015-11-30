1958960ee23b3225fa895044d6464848532fac72
JohnyWalk3r <johny_walker@windowslive.com>
2015-11-30 14:25:22 +0000

    Initial commit

--- yolo.php
+++ yolo.php
@@ -9,6 +9,20 @@
 <header>
     <h1>Header Div</h1>
 </header>
+<script>
+    window.onload = function() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                document.getElementById("twitter").innerHTML = xhttp.responseText;
+            }
+        };
+        xhttp.open("GET", "http://nodeappl.azurewebsites.net    ", true);
+        xhttp.send();
+    }
+
+</script>
+
 <main>
     <h2> Main content </h2>
 </main>

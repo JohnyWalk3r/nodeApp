7a6684ec153ba11414dffacdf79dc054e516b5d2
JohnyWalk3r <johny_walker@windowslive.com>
2015-11-30 14:20:14 +0000

    Initial commit

---
+++ yolo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Consuming Twitter Webserver</title>
+
+</head>
+<body>
+<header>
+    <h1>Header Div</h1>
+</header>
+<main>
+    <h2> Main content </h2>
+</main>
+<aside id="twitter" >
+
+</aside>
+</body>
+</html>
